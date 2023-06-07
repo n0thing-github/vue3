f9aec7bd8544090c88eb44114c454f20de37f744
yao <a13030242239@126.com>
2023-06-07 22:48:22 +0800

    layout渲染一级导航列表

--- src/views/Layout/components/LayoutHead.vue
+++ src/views/Layout/components/LayoutHead.vue
@@ -1,5 +1,18 @@
 <script setup>
+import { getCategoryAPI } from '@/apis/layout'
+import { onMounted, ref } from 'vue';
 
+const categoryList = ref([])
+const getCategory = async () => {
+    const res = await getCategoryAPI()
+    console.log(res)
+    categoryList.value = res.result
+
+}
+
+onMounted(()=>{
+    getCategory()
+})
 </script>
 
 <template>
@@ -8,13 +21,10 @@
       <h1 class="logo">
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
-      <ul class="app-header-nav">
+      <ul class="app-header-nav" v-for="item in categoryList" :key="item.id">
         <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
